48eff763404e9a07f641da9e6b1784bfbeee3f86
Wonmobo <37849971+wonmobo@users.noreply.github.com>
2019-12-26 16:33:50 -0600

    relational_operators.py

--- relational_operators.py
+++ relational_operators.py
@@ -1,3 +1,6 @@
+import os
+import datetime
+
 def time_apart(departure_date, todays_date):
     day_count = todays_date - departure_date
     if day_count >= 5:
@@ -10,4 +13,5 @@ todays_date = int(input("What is today's date:  "))
 
 print(time_apart(departure_date, todays_date))
 
-
+v = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print(v)
