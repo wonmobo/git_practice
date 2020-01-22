c7ef5d1d9e7ef05826df752f4d458af96f2d06d3
Wonmobo <37849971+wonmobo@users.noreply.github.com>
2020-01-22 13:06:07 -0600

    delete a test.txt file

---
+++ organizeDesktop/_clean.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import datetime
+
+
+### Creating variables date / time and for moving files on my desktop ###
+now = str(datetime.datetime.now())[:19]
+now = now.replace(":","_")
+source = 'C:\\users\\ischlesinger\\Desktop\\'
+dest1 = 'C:\\users\\ischlesinger\\Desktop\\Recycle_Bin\\'
+files = os.listdir(source)
+
+### If file is not "Recycle_Bin" nor "clean.py", add today's date / time and move from Desktop to Recycle_Bin ###
+for f in files:
+    if not f.startswith('_'):
+        if not os.path.exists(dest1+f):
+            shutil.move(source+f, dest1)
+        else:
+            rename = source+str("("+now+") ")+f
+            shutil.move(source+f, rename)
+            shutil.move(rename, dest1)
+
+   
+
+
+### Test Code:
+
+### Move clean.py file from "Recycle_Bin" to my desktop ###   
+#if not os.path.exists('C:\\users\\ischlesinger\\Desktop\\clean.py'):
+    #shutil.move('C:\\users\\ischlesinger\\Desktop\\Recycle_Bin\\clean.py', #'C:\\users\\ischlesinger\\Desktop\\clean.py')
+    
+### If Recycle_Bin folder doesn't exist on my Desktop, create it ###
+#if not os.path.exists('C:\\users\\ischlesinger\\Desktop\\Recycle_Bin\\'):
+    #os.makedirs('C:\\users\\ischlesinger\\Desktop\\Recycle_Bin\\')
